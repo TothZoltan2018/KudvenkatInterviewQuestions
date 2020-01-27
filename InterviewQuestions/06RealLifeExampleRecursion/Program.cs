22909f7b233cb00477c16cb564fd0a70f18d1a64
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-01-27 13:58:24 +0100

    _06RealLifeExampleRecursion: Find and count files recursively

---
+++ InterviewQuestions/06RealLifeExampleRecursion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _06RealLifeExampleRecursion
+{
+    class Program
+    {
+        static int fileCounter = 0;
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Please enter a filepath to find all files:");
+            var path = Console.ReadLine();
+            FindFiles(path);
+            Console.WriteLine($"--- Total number of files: {fileCounter} ---");
+
+            Console.ReadLine();
+        }
+
+        private static void FindFiles(string path)
+        {
+            foreach (var fileName in Directory.GetFiles(path))
+            {
+                Console.WriteLine(fileName);
+                fileCounter++;
+            }
+
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                //Recursive call
+                FindFiles(directory);
+            }            
+        }
+    }
+}
