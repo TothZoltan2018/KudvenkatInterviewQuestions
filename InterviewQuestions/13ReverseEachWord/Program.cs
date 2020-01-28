edfdece2a1c5ade71d01ca1a11193abe1f41883a
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-01-28 17:22:16 +0100

    13ReverseEachWord

---
+++ InterviewQuestions/13ReverseEachWord/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13ReverseEachWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "one two three four five";
+
+            var reversedString = string.Join(" ",  inputString
+                .Split(' ')
+                .Select(x => new String(x.Reverse().ToArray())));
+            
+            Console.WriteLine(reversedString);
+            Console.ReadLine();
+        }
+    }
+}
